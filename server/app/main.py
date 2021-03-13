9bb81f1c214075ecabad945799cfcbe2e46a3450
Andrew <andrewscaldwell@gmail.com>
2021-03-13 12:05:27 -0500

    first code commit with flask tested boilerplate

--- server/app/main.py
+++ server/app/main.py
@@ -1,1 +1,17 @@
-#code will be written here
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate(".\sdkkey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#you can use this in place of db.collection('pets') since everything is in pets
+root_collection = db.collection(u'pets')
