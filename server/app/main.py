f79038050dc1024eec041dfd16e9473d72249a54
arifire21 <ariwolfpup@icloud.com>
2021-03-13 13:28:11 -0500

    commit for authtool

--- server/app/main.py
+++ server/app/main.py
@@ -3,13 +3,15 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import firebase_admin
 from firebase_admin import credentials, firestore
+from authtool import make_auth, check_hash
 
+# Firebase variables (global).
 cred = credentials.Certificate(".\sdkkey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# Flask variables (global).
 app = Flask(__name__)
-
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
