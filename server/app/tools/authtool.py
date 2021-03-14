bc30620c4454f86c7f6bfaed3d9e41421f3f0592
R. Hernandez <har@usf.edu>
2021-03-13 22:24:44 -0500

    Added (commented) crypto driver.

--- server/app/tools/authtool.py
+++ server/app/tools/authtool.py
@@ -22,3 +22,33 @@ def check_hash(username:str, passwd:str, salt:str) -> str:
 def _to_bytes(username:str, password:str) -> bytes:
     
     return (username + password).encode('utf-8')
+
+''' Do NOT use the crypt library (more secure) on Windows. It will break.
+import crypt
+from hmac import compare_digest
+
+"""
+Generates a new salt, hash tuple.
+"""
+def make_auth(username:str, passwd:str) -> tuple:
+
+    userpass = username + passwd
+    
+    salt = crypt.mksalt(crypt.METHOD_SHA512)
+    pwhash = crypt.crypt(userpass, salt)
+    
+    return salt, pwhash
+
+"""
+username and passwd should come from the frontend request.
+salt should come from the document field ("salt").
+"""
+def check_hash(username:str, passwd:str, salt:str) -> str:
+    
+    return crypt.crypt(username + passwd, salt)
+
+# Convert the concatenated user-pass to bytes-like object.
+def _to_bytes(username:str, password:str) -> bytes:
+    
+    return (username + password).encode('utf-8')
+'''
